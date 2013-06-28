f93d45a1d3db1759146e3f7ff21336454f0712bc
David Lin <david.lin@estimateone.com>
2013-06-28 10:12:09 +1000

    updated the namespace

--- DependencyInjection/DlinSnappyExtension.php
+++ DependencyInjection/DlinSnappyExtension.php
@@ -21,7 +21,7 @@ use Symfony\Component\DependencyInjection\Loader;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class DlinSnappyBundleExtension extends Extension
+class DlinSnappyExtension extends Extension
 {
 
     /**
